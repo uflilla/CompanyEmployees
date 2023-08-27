1b05327e8206b2f581bd48145cf1cc4ce89d81d5
uflilla <uflilla@gmail.com>
2023-08-27 21:20:11 +0500

    Repository Mechanism created.

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Service;
+using Services.Contracts;
 
 namespace CompanyEmployees.Extensions
 {
@@ -23,5 +27,15 @@ namespace CompanyEmployees.Extensions
 
     public static void ConfigureLoggerService(this IServiceCollection services) =>
       services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+      services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+    public static void ConfigureServiceManager(this IServiceCollection services) =>
+      services.AddScoped<IServiceManager, ServiceManager>();
+
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+      services.AddDbContext<RepositoryContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
   }
 }
--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
   public class RepositoryContext : DbContext
   {
+    public RepositoryContext(DbContextOptions options): base(options)
+    {
+      
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+      modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+    }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Employee> Employees { get; set; }
   }
 }
